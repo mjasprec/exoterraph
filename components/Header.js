c22a2e5bdefd1ffc4ba1a2c875cfc23c98550b48
mjasprec14 <michaeljosephasprec1@gmail.com>
2021-09-16 01:00:16 +0800

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import { HomeIcon, SearchIcon, PlusIcon, StarIcon } from "@heroicons/react";
+
+const Header = () => {
+  return (
+    <div className="stick top-0 z-[1000] bg-[#040714] h-[72px] flex items-center px-10 md:px-12">
+      <Image src="/images/logo.svg" width={80} height={80} />
+
+      <div className="hidden ml-10 md:flex items-center space-x-6">
+        <div className="hidden ml-10 md:flex items-center space-x-6">
+          <a className="group header-link">
+            <HomeIcon className="h-4" />
+            <span className="span">Home</span>
+          </a>
+
+          <a className="header-link group">
+            <SearchIcon className="h-4" />
+            <span className="span">Search</span>
+          </a>
+
+          <a className="header-link group">
+            <PlusIcon className="h-4" />
+            <span className="span">Watchlist</span>
+          </a>
+
+          <a className="header-link group">
+            <StarIcon className="h-4" />
+            <span className="span">Originals</span>
+          </a>
+
+          <a className="header-link group">
+            <img src="/images/movie-icon.svg" alt="" className="h-5" />
+            <span className="span">Movies</span>
+          </a>
+
+          <a className="header-link group">
+            <img src="/images/series-icon.svg" alt="" className="h-5" />
+            <span className="span">Series</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

db7cf9177a6e9ead43cfe1169e258c8a98ec09d4
mjasprec14 <michaeljosephasprec1@gmail.com>
2021-09-16 12:12:12 +0800

    fix: auth import

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
 import { db } from "../../../firebase";
